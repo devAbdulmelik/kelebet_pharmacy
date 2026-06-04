0d1fe7105954701b7b0b83efcb3d26060d342e68
devAbdulmelik <abdumel6@gmail.com>
2026-06-04 18:00:32 +0300

    Serve Django static files in production

--- kelebet_pharmacy/settings.py
+++ kelebet_pharmacy/settings.py
@@ -79,6 +79,7 @@ AUTH_USER_MODEL = 'api.CustomUser'
 MIDDLEWARE = [ 
     'corsheaders.middleware.CorsMiddleware',   # Must be near the top
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -165,6 +166,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # REST Framework Settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
