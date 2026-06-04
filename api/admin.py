530b5d66b3f6117fe5f6579379198bad9072ec06
devAbdulmelik <abdumel6@gmail.com>
2026-06-04 17:29:20 +0300

    Polish Django admin interface

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff', 'is_active')
+    list_filter = ('role', 'is_staff', 'is_active')
+    search_fields = ('username', 'email', 'first_name', 'last_name')
+    ordering = ('username',)
+    fieldsets = UserAdmin.fieldsets + (
+        ('Pharmacy Profile', {'fields': ('role', 'phone', 'address', 'date_of_birth')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ('Pharmacy Profile', {'fields': ('role', 'email', 'phone')}),
+    )
+
+
+admin.site.site_header = 'Kelebet Pharmacy Admin'
+admin.site.site_title = 'Kelebet Pharmacy Admin'
+admin.site.index_title = 'Administration Console'
 
 
 
